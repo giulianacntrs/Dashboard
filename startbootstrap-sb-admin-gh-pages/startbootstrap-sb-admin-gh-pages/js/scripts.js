3b4b98454e797281c509a7638abcbf8a353dd37a
giulianacntrs <112666134+giulianacntrs@users.noreply.github.com>
2024-12-05 21:18:51 -0300

    Add files via upload

---
+++ startbootstrap-sb-admin-gh-pages/startbootstrap-sb-admin-gh-pages/js/scripts.js
@@ -0,0 +1,80 @@
+/*!
+    * Start Bootstrap - SB Admin v7.0.7 (https://startbootstrap.com/template/sb-admin)
+    * Copyright 2013-2023 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
+    */
+    // 
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
+
+/* Grafico presupuesto*/
+
+Vue.component('pie-chart', {
+    extends: VueChartJs.Pie,
+   mounted () {
+      this.renderChart({
+        labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
+        datasets: [
+          {
+            backgroundColor: [
+              '#41B883',
+              '#E46651',
+              '#00D8FF',
+              '#DD1B16'
+            ],
+            data: [40, 20, 80, 10]
+          }
+        ]
+      }, {
+        responsive: true, 
+        maintainAspectRatio: false,
+        pieceLabel: {
+          mode: 'percentage',
+          precision: 1
+        }
+      })
+   }
+    
+  })
+  
+  var vm = new Vue({
+    el: '.app',
+    data: {
+      show: false,
+      message: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [40, 39, 10, 40, 39, 80, 40]
+          }
+        ]
+      }
+    },
+    
+    mounted () {
+    setInterval(() => {
+        //this.fillData()
+     },  5000)
+    }
+  });
+  
